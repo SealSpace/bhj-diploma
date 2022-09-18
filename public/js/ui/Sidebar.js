2fb256cf182f774e06c49227eca73d325b321995
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-18 14:02:49 +0300

    Update Sidebar.js

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -17,18 +17,45 @@ class Sidebar {
    * переключает два класса для body: sidebar-open и sidebar-collapse
    * при нажатии на кнопку .sidebar-toggle
    * */
-  static initToggleButton() {
-
+   static initToggleButton() {
+    document.querySelector('.sidebar-toggle').addEventListener('click', (e) => {
+        e.preventDefault();
+        if (!document.body.classList.contains('sidebar-open')) {
+          document.body.classList.remove('sidebar-collapse');
+            document.body.classList.add('sidebar-open');
+        } else {
+            document.body.classList.remove('sidebar-open');
+            document.body.classList.add('sidebar-collapse');
+        }
+    });
   }
+
 
   /**
    * При нажатии на кнопку входа, показывает окно входа
    * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
+   * При нажатии на кнопку регистрации показывает окно регистрации
    * При нажатии на кнопку выхода вызывает User.logout и по успешному
    * выходу устанавливает App.setState( 'init' )
    * */
-  static initAuthLinks() {
+   static initAuthLinks() {
+    document.querySelector('.menu-item_register > a').onclick = e => {
+      e.preventDefault();
+      App.getModal('register').open();
+    };
 
+    document.querySelector('.menu-item_login > a').onclick = e => {
+      e.preventDefault();
+      App.getModal('login').open();
+    };
+
+    document.querySelector('.menu-item_logout > a').onclick = e => {
+      e.preventDefault();
+      User.logout((err, resp) => {
+        if (response && response.success) {
+          App.setState('init');
+        }
+      })
+    };
   }
 }
