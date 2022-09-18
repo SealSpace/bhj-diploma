b9ea2fd2f272b7d2948279adf7e370f863e1564e
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-18 14:01:01 +0300

    Update Entity.js

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -2,14 +2,21 @@
  * Класс Entity - базовый для взаимодействия с сервером.
  * Имеет свойство URL, равно пустой строке.
  * */
-class Entity {
+ class Entity {
+  static url = '';
   /**
    * Запрашивает с сервера список данных.
    * Это могут быть счета или доходы/расходы
    * (в зависимости от того, что наследуется от Entity)
    * */
+  
   static list(data, callback){
-
+    createRequest({
+      url: this.url,
+      method: 'GET',
+      data,
+      callback
+    });
   }
 
   /**
@@ -18,7 +25,12 @@ class Entity {
    * что наследуется от Entity)
    * */
   static create(data, callback) {
-
+    createRequest({
+      url: this.url,
+      method: 'PUT',
+      data,
+      callback
+    });
   }
 
   /**
@@ -26,6 +38,11 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static remove(data, callback ) {
-
+    createRequest({
+      url: this.url,
+      method: 'DELETE',
+      data,
+      callback
+    });
   }
 }
