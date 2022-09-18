5a7a614782d9f66282a97579635d3801459b9142
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-18 14:00:19 +0300

    Update createRequest.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,41 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest();
+    let formData = null;
+    let result = '';
 
+    xhr.responseType = 'json';
+
+    if (options.method === 'GET') {
+        if (options.data) {
+            result = '?';
+
+            for (let i in options.data) {
+                result += `${i}=${options.data[i]}&`; 
+            }   
+        }
+    } 
+    
+    else {
+        formData = new FormData();
+        for (let i in options.data) {
+        formData.append( i, options.data[i] ); 
+        }
+    }
+
+    try {
+        xhr.open(`${options.method}`, `${options.url}${result}`);
+        
+        xhr.send(formData);
+    }
+    
+    catch (err) {
+        options.callback(err);
+    }
+
+    
+    xhr.onload = function() {
+        options.callback(null, xhr.response);
+    }
 };
