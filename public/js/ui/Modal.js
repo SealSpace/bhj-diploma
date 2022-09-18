80be678afe1ac331f20ed2682f6c3a6209fa0df7
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-18 14:03:38 +0300

    Update Modal.js

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -11,8 +11,12 @@ class Modal {
    * Если переданный элемент не существует,
    * необходимо выкинуть ошибку.
    * */
-  constructor(element){
-
+   constructor(element) {
+    if (!element) {
+      throw new Error('Передан пустой параметр Element');
+    }
+    this.element = element;
+    this.registerEvents();
   }
 
   /**
@@ -20,28 +24,36 @@ class Modal {
    * должен закрыть текущее окно
    * (с помощью метода Modal.onClose)
    * */
-  registerEvents() {
-
+   registerEvents() {
+    const dismiss = this.element.querySelectorAll('[data-dismiss="modal"]');
+    dismiss.forEach(element => {
+      element.addEventListener('click', (ev) => {
+        this.onClose(ev);
+      })
+    });
   }
 
   /**
    * Срабатывает после нажатия на элементы, закрывающие окно.
    * Закрывает текущее окно (Modal.close())
    * */
-  onClose(e) {
+   onClose(e) {
+    e.preventDefault();
+    this.close();
+   }
 
-  }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+    this.element.style.display = 'block';
   }
+
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
   close(){
-
+    this.element.style.removeProperty('display');
   }
 }
