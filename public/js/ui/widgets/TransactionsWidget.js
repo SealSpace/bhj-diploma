03fd5de7e5c5da12ff57fcfefb57126919c7512a
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-26 12:39:40 +0300

    Update TransactionsWidget.js

--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -4,7 +4,7 @@
  * создания нового дохода или расхода
  * */
 
-class TransactionsWidget {
+ class TransactionsWidget {
   /**
    * Устанавливает полученный элемент
    * в свойство element.
@@ -12,7 +12,9 @@ class TransactionsWidget {
    * необходимо выкинуть ошибку.
    * */
   constructor( element ) {
-
+    if (!element) console.error('элемент не существует');
+    this.element = element;
+    this.registerEvents();
   }
   /**
    * Регистрирует обработчики нажатия на
@@ -21,6 +23,13 @@ class TransactionsWidget {
    * экземпляра окна
    * */
   registerEvents() {
-
+    this.element.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (e.target.classList.contains('create-income-button')) {
+          App.getModal('newIncome').open();
+      } else if (e.target.classList.contains('create-expense-button')) {
+          App.getModal('newExpense').open();
+      }
+    });
   }
 }
