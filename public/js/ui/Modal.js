067b70df302c55869809f7c4d57488acb7259427
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-26 16:20:05 +0300

    Update Modal.js

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -1,59 +1,54 @@
 /**
- * Класс Modal отвечает за
- * управление всплывающими окнами.
- * В первую очередь это открытие или
- * закрытие имеющихся окон
- * */
+* Класс Modal отвечает за
+* управление всплывающими окнами.
+* В первую очередь это открытие или
+* закрытие имеющихся окон
+* */
 class Modal {
-  /**
-   * Устанавливает текущий элемент в свойство element
-   * Регистрирует обработчики событий с помощью Modal.registerEvents()
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
-   constructor(element) {
-    if (!element) {
-      throw new Error('Передан пустой параметр Element');
-    }
-    this.element = element;
-    this.registerEvents();
-  }
+ /**
+  * Устанавливает текущий элемент в свойство element
+  * Регистрирует обработчики событий с помощью Modal.registerEvents()
+  * Если переданный элемент не существует,
+  * необходимо выкинуть ошибку.
+  * */
+ constructor(element){
+   if (!element) console.error('Передан пустой элемент в конструктор!');
+   this.element = element;
+   this.registerEvents();
+ }
 
-  /**
-   * При нажатии на элемент с data-dismiss="modal"
-   * должен закрыть текущее окно
-   * (с помощью метода Modal.onClose)
-   * */
-   registerEvents() {
-    const dismiss = this.element.querySelectorAll('[data-dismiss="modal"]');
-    dismiss.forEach(element => {
-      element.addEventListener('click', (ev) => {
-        this.onClose(ev);
-      })
-    });
-  }
+ /**
+  * При нажатии на элемент с data-dismiss="modal"
+  * должен закрыть текущее окно
+  * (с помощью метода Modal.onClose)
+  * */
+ registerEvents() {
+   this.element.querySelectorAll('[data-dismiss="modal"]').forEach(
+     el => el.addEventListener('click', (e) => {
+         this.onClose(e);
+     })
+   );
+ }
 
-  /**
-   * Срабатывает после нажатия на элементы, закрывающие окно.
-   * Закрывает текущее окно (Modal.close())
-   * */
-   onClose(e) {
-    e.preventDefault();
-    this.close();
-   }
-
-  /**
-   * Открывает окно: устанавливает CSS-свойство display
-   * со значением «block»
-   * */
-  open() {
-    this.element.style.display = 'block';
-  }
-
-  /**
-   * Закрывает окно: удаляет CSS-свойство display
-   * */
-  close(){
-    this.element.style.removeProperty('display');
-  }
+ /**
+  * Срабатывает после нажатия на элементы, закрывающие окно.
+  * Закрывает текущее окно (Modal.close())
+  * */
+ onClose(e) {
+   e.preventDefault();
+   this.close();
+ }
+ /**
+  * Открывает окно: устанавливает CSS-свойство display
+  * со значением «block»
+  * */
+ open() {
+   this.element.style.display = 'block';
+ }
+ /**
+  * Закрывает окно: удаляет CSS-свойство display
+  * */
+ close(){
+   this.element.style.removeProperty('display');
+ }
 }
