d685b4a9d5016a3d3ba648c8b35ff9021732dbd4
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-26 16:20:30 +0300

    Update Sidebar.js

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -3,7 +3,7 @@
  * кнопки скрытия/показа колонки в мобильной версии сайта
  * и за кнопки меню
  * */
-class Sidebar {
+ class Sidebar {
   /**
    * Запускает initAuthLinks и initToggleButton
    * */
@@ -17,7 +17,7 @@ class Sidebar {
    * переключает два класса для body: sidebar-open и sidebar-collapse
    * при нажатии на кнопку .sidebar-toggle
    * */
-   static initToggleButton() {
+  static initToggleButton() {
     document.querySelector('.sidebar-toggle').addEventListener('click', (e) => {
         e.preventDefault();
         if (!document.body.classList.contains('sidebar-open')) {
@@ -30,32 +30,27 @@ class Sidebar {
     });
   }
 
-
   /**
    * При нажатии на кнопку входа, показывает окно входа
    * (через найденное в App.getModal)
-   * При нажатии на кнопку регистрации показывает окно регистрации
+   * При нажатии на кнопку регастрации показывает окно регистрации
    * При нажатии на кнопку выхода вызывает User.logout и по успешному
    * выходу устанавливает App.setState( 'init' )
    * */
-   static initAuthLinks() {
-    document.querySelector('.menu-item_register > a').onclick = e => {
-      e.preventDefault();
-      App.getModal('register').open();
-    };
-
-    document.querySelector('.menu-item_login > a').onclick = e => {
-      e.preventDefault();
-      App.getModal('login').open();
-    };
-
-    document.querySelector('.menu-item_logout > a').onclick = e => {
-      e.preventDefault();
-      User.logout((err, resp) => {
-        if (response && response.success) {
-          App.setState('init');
-        }
-      })
-    };
+  static initAuthLinks() {
+    document.querySelector('.menu-item_register > a').addEventListener('click', (e) => {
+        e.preventDefault();
+        App.getModal('register').open();
+      });
+    document.querySelector('.menu-item_login > a').addEventListener('click', (e) => {
+        e.preventDefault();
+        App.getModal('login').open();
+      });
+    document.querySelector('.menu-item_logout > a').addEventListener('click', (e) => {
+        e.preventDefault();
+        User.logout((err, response) => {
+          if (response && response.success) App.setState('init');
+        });
+      });
   }
 }
