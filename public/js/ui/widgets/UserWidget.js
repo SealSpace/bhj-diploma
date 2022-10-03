eef9772c3786f75fd2fea6ccea9d82c1d16b0d8c
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-10-03 14:22:18 +0300

    Update UserWidget.js

--- public/js/ui/widgets/UserWidget.js
+++ public/js/ui/widgets/UserWidget.js
@@ -4,7 +4,7 @@
  * после авторизации или его выхода из системы
  * */
 
-class UserWidget {
+ class UserWidget {
   /**
    * Устанавливает полученный элемент
    * в свойство element.
@@ -12,7 +12,9 @@ class UserWidget {
    * необходимо выкинуть ошибку.
    * */
   constructor(element){
-    if (!element) console.error('Передан пустой элемент');
+    if (!element) {
+      throw new Error('данные пользователя отсутствуют');
+    }
     this.element = element;
   }
 
